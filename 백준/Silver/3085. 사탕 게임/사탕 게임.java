d02ee7d5e567d061e8115815aabd6abd1382d59f
gangwoo <71250100+khyme1022@users.noreply.github.com>
2023-08-08 15:12:43 +0900

    [Silver II] Title: 사탕 게임, Time: 144 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,70 +1,92 @@
-import java.util.Scanner;
-public class Main {
-	static int max = 1, N = 0;
-	static char arr[][];
-	
-	public static void swap(int x1, int y1, int x2, int y2) { 
-		char tmp = arr[x1][y1];
-		arr[x1][y1] = arr[x2][y2];
-		arr[x2][y2] = tmp; 
-	}
-	
-	public static int chkRow(int x) { // 행 체크
-		int tmp = 1, res = 1;
-		char ch = arr[x][0];
-		for(int i = 1; i < N; i++) {
-			if(arr[x][i] != ch) {
-				ch = arr[x][i];
-				res = Math.max(res, tmp);
-				tmp = 1;
-			} else tmp++;
-		}
-		return Math.max(res,  tmp);
-	}
-	
-	public static int chkCol(int y) { // 열 체크
-		int tmp = 1, res = 1;
-		char ch = arr[0][y];
-		for(int i = 1; i < N; i++) {
-			if(arr[i][y] != ch) {
-				ch = arr[i][y];
-				res = Math.max(res, tmp);
-				tmp = 1;
-			} else tmp++;
-		}
-		return Math.max(res, tmp);
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		arr = new char[N][N];
-		for(int i = 0; i < N; i++) {
-			String tmp = sc.next();
-			for(int j = 0; j < N; j++) arr[i][j] = tmp.charAt(j);
-			max = Math.max(max, chkRow(i)); // swap하기 전 행 체크			
-		}
-		
-		for(int i = 0; i < N; i++) max = Math.max(max, chkCol(i)); // swap하기 전 열 체크
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(j + 1 < N) { // 열 범위가 벗어나지 않을 때
-					swap(i, j, i, j + 1); // 오른쪽이랑 swap
-					max = Math.max(max, chkRow(i));
-					max = Math.max(max, chkCol(j));
-					max = Math.max(max, chkCol(j + 1));
-					swap(i, j, i, j + 1); // 복귀
-				}
-				if(i + 1 < N) { // 행 범위가 벗어나지 않을 때
-					swap(i, j, i + 1, j); // 아래쪽이랑 swap
-					max = Math.max(max, chkRow(i));
-					max = Math.max(max, chkRow(i + 1));
-					max = Math.max(max, chkCol(j));
-					swap(i, j, i + 1, j); // 복귀
-				}
-			}
-		}
-		System.out.println(max);
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+
+    static char[][] graph;
+
+    static int N;
+    static int max;
+    public static void main(String args[])  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine()); // 줄수
+        graph = new char[N][N];
+
+        String s;
+        for (int i = 0; i < N; i++) {
+            s = br.readLine();
+            graph[i] = s.toCharArray();
+        } // 표에 넣기
+
+        max = 1;
+
+        check(0,N-1,0,N-1);
+        for(int i=0; i<N;i++){
+            for(int j=0; j<N;j++){
+                if(j+1<N){
+                    swap(i,j,i,j+1);
+                    check(i,j,i,j+1);
+                    swap(i,j,i,j+1);
+                }
+                if(i+1<N){
+                    swap(i,j,i+1,j);
+                    check(i,j,i+1,j);
+                    swap(i,j,i+1,j);
+                }
+            }
+        }
+
+        System.out.println(max);
+
+    }
+    static void swap(int a, int b, int x, int y){ // 교환 함수
+        char temp;
+        temp = graph[a][b];
+        graph[a][b] = graph[x][y];
+        graph[x][y] = temp;
+
+
+    }
+
+    static void check(int a, int b, int x, int y){ // 확인하는 함수
+        int result = 1;
+        for(int i=a;i<=x;i++){ // 수직
+            result = 1;
+            for(int j=0;j<N-1;j++){
+
+                if(graph[i][j]==graph[i][j+1]){
+                    result++;
+                }
+                else result = 1;
+                max = Math.max(max, result);
+            }
+        }
+        for(int j =b;j<=y;j++){ // 수평 검사
+            result = 1;
+            for(int i=0;i<N-1;i++){
+
+                if(graph[i][j]==graph[i+1][j]) {
+                    result++;
+                }
+                else result = 1;
+                max = Math.max(max, result);
+
+            }
+        }
+    }
 }
+
+// 최장 길이 기록
+// 교체전에 행과 열을 한번 씩 체크한다
+// 상하 교체 시 해당 열과 교체된 각각 행 체크
+// 좌우 교체 시 해당 행과 교체된 각각 열 체크
+// 오른쪽 막힐 때
+// 아래 막힐 때
+
+
+
+
+// 상하 교체 시 - 교체한 행의 가로 최장 체크.
+// 좌우 교체 시 - 교체한 열의 세로 최장 체크.
